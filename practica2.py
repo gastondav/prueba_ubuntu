a1ad9a85d1f922f853954eed438bc7a864936575
gaston <gastondavalos123@gmail.com>
2023-08-20 23:37:35 -0300

    subida 3

--- practica2.py
+++ practica2.py
@@ -23,7 +23,65 @@ def factorial_aux(n):
       return (factorial)
 
 def fact_m():
-      m = int(input("Ingrese un valor: "))
+      m = int(input("Ingrese la cantidad de numeros: "))
       for i in range (1, m + 1):
-            valor = factorial_aux(i)
-            print(valor)
+            n = int (input ("Ingrese un numero: "))
+            valor = factorial_aux(n)
+            print(str(n) + "! = " + str(valor))
+
+# Ejercicio 5
+
+def farCel (f) :
+    return (f -32) *5/9
+
+def tabla():
+      for i in range (0,121,10):
+            print (farCel (i))
+
+# Ejercicio 6
+
+def suma_n(n):
+      suma = 0
+      for i in range(0,n+1):
+            suma = suma + i
+      return suma
+
+def triang(m):
+      for i in range (1, m+1):
+            valor = suma_n(i)
+            print(str(i) + " - " + str(valor))
+            
+# Ejercicio 7
+
+def pos():
+      n = int (input ("Ingrese un numero positivo: "))    
+      while not (n > 0):
+            n = int (input ("Error, volver a ingresar un numero positivo: "))
+
+# Ejercicio 8
+
+def ingresar():
+      l=[]
+      contador = 0
+      print("Ingresar Notas: ")
+      valor = True
+      nota = int (input ("Ingrese una nota: "))
+      l[0:0]= {nota}
+      while valor:
+        valor = input ("Quiere ingresar una nota: ")
+        if valor == "no":
+              valor = False
+        else: 
+              nota = int (input ("Ingrese una nota: "))
+              l[0:0]= {nota}
+              contador = contador + 1
+      print (prom (l,contador))
+
+def prom(l,contador):   # calcula el promedio, recibe la lista y el largo de la misma
+      suma = 0
+      for i in range (0, contador+1):
+            suma = suma + l[i]
+      print("El promedio es: " + str (suma))
+
+# Ejercicio 9
+
